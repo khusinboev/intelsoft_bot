f4518ba11e17454264e19766198fd330616dc36c
Windows 10 <adhambekhusinboyev@gmail.com>
2024-07-10 13:04:06 +0500

    first commit

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+from pathlib import Path
+
+BASE_DIR = str(Path(__file__).resolve().parent)
+
 TOKEN = "5597490636:AAHiyobYfWKIqdzd8CDPzHoaZes5EiWr1pw"
 GROUP_ID = -1002153624501
-DB_NAME = 'intel_soft.db'
+DB_NAME = BASE_DIR + '/intel_soft.db'
+
