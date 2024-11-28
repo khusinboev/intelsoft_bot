45c84f94cc84c35e5a719bf9c29434d1e8fea44c
Windows 10 <adhambekhusinboyev@gmail.com>
2024-11-28 20:18:53 +0500

    first commit

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ from pathlib import Path
 BASE_DIR = str(Path(__file__).resolve().parent)
 
 TOKEN = "7470635147:AAEM6Z1K7CKD2LDBZ9vTAs7fWUsoqsIldDM"
-GROUP_ID = -1002153624501
+GROUP_ID = -1002431288328
 DB_NAME = BASE_DIR + '/intel_soft.db'
 
 bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
